d371f3a706548adb925d9f3b0345cdb51384e0fe
rubens leme <rubenss.leme@gmail.com>
2019-04-14 14:32:38 -0300

    Implementação da da leitura de argumento na execussão do programa.

--- src/main/java/appRotaDeViagem/TesteRoute.java
+++ src/main/java/appRotaDeViagem/TesteRoute.java
@@ -1,6 +1,7 @@
 package appRotaDeViagem;
 
-public class Principal {
+public class TesteRoute {
+
 	public static void main(String[] args) {
 
 		for (int i = 0; i < args.length; i++) {
@@ -9,16 +10,14 @@ public class Principal {
 				System.out.println("Rotas carregadas..\n" + "Entre com a rota desejada:<saida-destino>Ex.: GRU-CGD");
 				break;
 			}
-			default : {
-				String s = args[i].toString();
-				System.out.println("\nRota digitada: " + s);
-				
-				System.out.println("\nEntre com a proxima rota desejada:<saida-destino> Ex.: GRU-CGD");
+			case "-p": {
+				System.out.println("Entre com a proxima rota desejada:<saida-destino> Ex.: GRU-CGD");
 				break;
 			}
 
 			}
 
 		}
+
 	}
 }
